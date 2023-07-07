2c495c32743566ec7b52df4cdd76900aa2eaa59d
Klecianny Melo <kleciannymelo@gmail.com>
2023-07-07 14:30:46 -0300

    :construction: aula 2: array forEach

---
+++ 2.Array-forEach.js
@@ -0,0 +1,30 @@
+const animals = [
+    { name: 'Leão', emoji: '🦁', age: 5 },
+    { name: 'Elefante', emoji: '🐘', age: 10 },
+    { name: 'Girafa', emoji: '🦒', age: 7 },
+    { name: 'Tubarão', emoji: '🦈', age: 15 },
+    { name: 'Golfinho', emoji: '🐬', age: 8 },
+    { name: 'Orca', emoji: '🐋', age: 12 },
+    { name: 'Águia', emoji: '🦅', age: 3 },
+    { name: 'Coruja', emoji: '🦉', age: 4 },
+    { name: 'Pinguim', emoji: '🐧', age: 6 }
+];
+
+/* Exebindo todos os dados */
+// for (let i = 0; i < animals.length; i++) {
+//     console.log(animals[i]);
+// }
+
+/* Exebindo os dados com forEach */
+// animals.forEach((animal) => console.log(animal));
+
+/* Somando todas as idades */
+let ageSum = 0;
+
+animals.forEach((animal) => {
+    ageSum += animal.age;
+});
+
+console.log("ageSum: ", ageSum);
+
+/* ESTOU EM: seção 2 - aula 6 - 3:06 min */
