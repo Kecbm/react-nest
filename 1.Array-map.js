6ef65164f1ca978b3e525c4abf98028c391b763f
Klecianny Melo <kleciannymelo@gmail.com>
2023-07-07 14:12:47 -0300

    :hammer: descomentando o código

--- 1.Array-map.js
+++ 1.Array-map.js
@@ -42,36 +42,36 @@ const persons = [
 ];
 
 /* Exibe o nome e idade de cada objeto */
-// console.log("persons: ", persons);
+console.log("persons: ", persons);
 
 /* Exebindo somente a idade */
-// const agePersons = persons.map((person) => person.age);
-// console.log("agePersons: ", agePersons);
+const agePersons = persons.map((person) => person.age);
+console.log("agePersons: ", agePersons);
 
 /* Exibe o nome */
-// const namePersons = persons.map((person) => person.name);
-// console.log("namePerson: ", namePersons);
+const namePersons = persons.map((person) => person.name);
+console.log("namePerson: ", namePersons);
 
 /* Concatenando as informações */
-// const allInfosPersons = persons.map((person) => {
-//     return {
-//         name: person.name,
-//         age: person.age,
-//         nameAge: `${person.name} - ${person.age}`
-//     }
-// }
-// );
-// console.log("allInfosPersons: ", allInfosPersons);
+const allInfosPersons = persons.map((person) => {
+    return {
+        name: person.name,
+        age: person.age,
+        nameAge: `${person.name} - ${person.age}`
+    }
+}
+);
+console.log("allInfosPersons: ", allInfosPersons);
 
 /* Spreed do objeto: trás todas as informações contidas nele */
-// const allPersonsNow = persons.map((person) => {
-//     return {
-//         ...person,
-//         dateNow: new Date()
-//     }
-// }
-// );
-// console.log("allPersonsNow: ", allPersonsNow);
+const allPersonsNow = persons.map((person) => {
+    return {
+        ...person,
+        dateNow: new Date()
+    }
+}
+);
+console.log("allPersonsNow: ", allPersonsNow);
 
 /* Substituindo uma chave do objeto original */
 const newNamePersons = persons.map((person, index) => {
