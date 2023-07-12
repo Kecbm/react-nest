f8ee70bd550b1b1168f60999b655d765d43c128b
Klecianny Melo <kleciannymelo@gmail.com>
2023-07-11 21:51:58 -0300

    :construction: aula sobre array find

---
+++ 4.Array-find.js
@@ -0,0 +1,54 @@
+const musicalInstruments = [
+    {
+      name: "Violão",
+      emoji: "🎸",
+      inventor: "Antonio de Torres",
+    },
+    {
+      name: "Piano",
+      emoji: "🎹",
+      inventor: "Bartolomeo Cristofori",
+    },
+    {
+      name: "Bateria",
+      emoji: "🥁",
+      inventor: "William Ludwig",
+    },
+    {
+      name: "Guitarra",
+      emoji: "🎶",
+      inventor: "Adolph Rickenbacker",
+    },
+    {
+      name: "Baixo",
+      emoji: "🎸",
+      inventor: "Leo Fender",
+    },
+    {
+      name: "Saxofone",
+      emoji: "🎷",
+      inventor: "Adolphe Sax",
+    },
+    {
+      name: "Violino",
+      emoji: "🎻",
+      inventor: "Andrea Amati",
+    },
+    {
+      name: "Trompete",
+      emoji: "🎺",
+      inventor: "Antoine Courtois",
+    },
+    {
+      name: "Flauta",
+      emoji: "🎵",
+      inventor: "Theobald Boehm",
+    }
+];
+
+/* Buscando o saxofone */
+// const searchSax = musicalInstruments.find((instrument) => instrument.name === "Saxofone");
+
+// console.log("searchSax: ", searchSax);
+
+/* ESTOU EM: Seção 2 - aula 8 - 1:18 min */
