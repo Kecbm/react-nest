6a14078574b1f5e50225d4459006d855e479f2d6
Klecianny Melo <kleciannymelo@gmail.com>
2023-07-13 22:50:48 -0300

    :construction: array para a aula de array sort

---
+++ 7.array_sort.js
@@ -0,0 +1,39 @@
+const greatestVictoriesHamilton = [
+    {
+      year: 2008,
+      location: "Silverstone, United Kingdom",
+      carType: "McLaren MP4-23",
+      team: "McLaren",
+      emoji: "🇬🇧"
+    },
+    {
+      year: 2012,
+      location: "Austin, United States",
+      carType: "McLaren MP4-27",
+      team: "McLaren",
+      emoji: "🇺🇸"
+    },
+    {
+      year: 2017,
+      location: "Montreal, Canada",
+      carType: "Mercedes F1 W08 EQ Power+",
+      team: "Mercedes",
+      emoji: "🇨🇦"
+    },
+    {
+      year: 2019,
+      location: "Suzuka, Japan",
+      carType: "Mercedes F1 W10 EQ Power+",
+      team: "Mercedes",
+      emoji: "🇯🇵"
+    },
+    {
+      year: 2020,
+      location: "Portimão, Portugal",
+      carType: "Mercedes F1 W11 EQ Performance",
+      team: "Mercedes",
+      emoji: "🇵🇹"
+    }
+];
+
+// ESTOU EM: Seção 2 - Aula 10
