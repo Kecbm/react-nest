1a06a374dc5b5930f9d4cbbf31bf0e7e0a0c9c4b
Klecianny Melo <kleciannymelo@gmail.com>
2023-07-17 22:25:49 -0300

    :construction: array para a aula de array flatMap

---
+++ 9.Array_flatMap.js
@@ -0,0 +1,56 @@
+const bestSellingBooks = [
+    {
+      title: "Dom Quixote",
+      author: "Miguel de Cervantes",
+      year: 1605,
+      emoji: "🎠"
+    },
+    {
+      title: "Um Conto de Duas Cidades",
+      author: "Charles Dickens",
+      year: 1859,
+      emoji: "🗺️"
+    },
+    {
+      title: "O Senhor dos Anéis",
+      author: "J.R.R. Tolkien",
+      year: 1954,
+      emoji: "💍"
+    },
+    {
+      title: "O Pequeno Príncipe",
+      author: "Antoine de Saint-Exupéry",
+      year: 1943,
+      emoji: "🤴🏾"
+    },
+    {
+      title: "Harry Potter e a Pedra Filosofal",
+      author: "J.K. Rowling",
+      year: 1997,
+      emoji: "🧙🏾‍♂️"
+    },
+    {
+      title: "O Alquimista",
+      author: "Paulo Coelho",
+      year: 1988,
+      emoji: "🔮"
+    },
+    {
+      title: "Cem Anos de Solidão",
+      author: "Gabriel García Márquez",
+      year: 1967,
+      emoji: "⌛"
+    },
+    {
+      title: "O Código Da Vinci",
+      author: "Dan Brown",
+      year: 2003,
+      emoji: "🖼️"
+    },
+    {
+      title: "O Apanhador no Campo de Centeio",
+      author: "J.D. Salinger",
+      year: 1951,
+      emoji: "🌾"
+    }
+];
